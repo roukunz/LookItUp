59456d008018ae929f452947fa517472b097c254
Jeffrey <roukunz@gmail.com>
2019-05-01 17:21:44 +1000

    Linked to mongodb, register, logout
    Imported new npms
    link new routes and database

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-var express = require("express");
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    methodOverride = require("method-override"),
+    localStrategy = require("passport-local"),
+    bodyParser = require("body-parser"),
+    flash = require("connect-flash"),
+    User = require("./models/user");
+
+
+mongoose.connect("mongodb://localhost/lookitup", { useNewUrlParser: true });
+
 var app = express();
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(flash());
+
 //routes
 var indexRoute = require("./route/index");
-
 
 app.set("view engine", "ejs");
 
@@ -12,8 +28,22 @@ app.use(require("express-session")({
     saveUnitialized: false
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+})
+
 app.use(express.static(__dirname + "/public"));
 app.use(indexRoute);
+
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.listen(3000, '0.0.0.0', function(){
     console.log("Server has started ....");
--- route/index.js
+++ route/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user")
 
 router.get('/', function(req,res){
     res.render("index");
@@ -13,4 +15,34 @@ router.get('/register', function(req,res){
     res.render("register");
 })
 
+router.post('/register', function(req,res){
+    var newUser = new User({forum_name: req.body.forumName,username: req.body.username });
+
+    User.findOne({username: req.body.username}, function(err,user){
+        if(user){
+            req.flash("error", "Username : " + req.body.username + "has been taken");
+            res.redirect("/register");
+        }
+       
+    })
+    User.findOne({forum_name : req.body.forumName}, function(err,user){
+        if(user){
+            req.flash("error", "ForumName : " + req.body.forumName + "has been taken");
+            res.redirect("/register");
+        }
+       
+    })
+    User.register(newUser,req.body.password, function(err,user){
+        passport.authenticate("local")(req,res,function(){
+            req.flash("success", "Welcome to Lookitup " + user.forum_name);
+            res.redirect("/");
+        })
+    })
+})
+
+router.get('/logout', function(req,res){
+    req.logout();
+    req.flash("success", "Successfully logged out!");
+    res.redirect("/")
+})
 module.exports = router;
