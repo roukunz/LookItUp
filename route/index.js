4357e31450556fb7fd838ac7e1751644d1df2ac9
Jeffrey <roukunz@gmail.com>
2019-05-14 16:55:03 +1000

    highest to lowest

--- route/index.js
+++ route/index.js
@@ -93,10 +93,10 @@ router.get('*', function(req,res){
 
 
 function compare( a, b ) {
-    if ( a.rating < b.rating ){
+    if ( a.rating > b.rating ){
       return -1;
     }
-    if ( a.rating > b.rating ){
+    if ( a.rating < b.rating ){
       return 1;
     }
     return 0;
